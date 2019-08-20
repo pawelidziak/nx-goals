50822f6984b494ad9b3377f0ea092c3827224d56
Paweł Idziak <pa.idziak@gmail.com>
2019-08-20 20:10:25 +0200

    Build/ngrx (#2)
    * build(ngrx): add ngrx libs from npm
    
    * build(angular): update angular cli to version 8.2.2

--- workspace/apps/angular-goals/src/app/app.module.ts
+++ workspace/apps/angular-goals/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { NxModule } from '@nrwl/angular';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
@@ -10,7 +16,11 @@ import { FooterComponent } from './layout/footer/footer.component';
   declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    NxModule.forRoot(),
+    RouterModule.forRoot([]),
+    StoreModule.forRoot({}, { metaReducers: !environment.production ? [storeFreeze] : [] }),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
