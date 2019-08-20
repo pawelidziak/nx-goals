558b41fdb37142fed9d3585e7f9212dbf4a29531
Pawel Idziak <pa.idziak@gmail.com>
2019-08-20 18:56:17 +0200

    feat(navbar): create navbar component

--- workspace/apps/angular-goals/src/app/app.module.ts
+++ workspace/apps/angular-goals/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
