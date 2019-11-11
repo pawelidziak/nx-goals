24990b6cc141d381769247d9a274c612d342fbd9
Pawel Idziak <pa.idziak@gmail.com>
2019-11-11 10:36:00 +0100

    feat(ngx forms, textarea): delete unused selector

--- workspace/libs/ngrx-forms/src/lib/fields/textarea/textarea.component.ts
+++ workspace/libs/ngrx-forms/src/lib/fields/textarea/textarea.component.ts
@@ -2,7 +2,6 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { BaseDynamicField } from '../base-dynamic-field';
 
 @Component({
-  selector: 'app-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
