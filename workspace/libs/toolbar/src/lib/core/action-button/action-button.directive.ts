7f8515604fe055623a25acf32d6bf21a5e022e15
Pawel Idziak <pa.idziak@gmail.com>
2019-11-24 12:34:49 +0100

    feat(toolbar, action directive): update action button directive logic

--- workspace/libs/toolbar/src/lib/core/action-button/action-button.directive.ts
+++ workspace/libs/toolbar/src/lib/core/action-button/action-button.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[toolbarActionButton]'
 })
-export class ActionButtonDirective {
+export class ActionButtonDirective implements AfterViewInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    this.addCssClass();
+    this.sorroundByElement('li');
+  }
 
+  addCssClass() {
+    const tagName = this.el.nativeElement.tagName.toLowerCase();
+    if (tagName === 'button' || tagName === 'a') {
+      this.el.nativeElement.classList.add('item__button');
+    }
+  }
+  
+  sorroundByElement(sourroundBy: string) {
+    // Get parent of the original input element
+    const parent = this.el.nativeElement.parentNode;
+
+    // Create a sourroundBy element
+    const liElement = this.renderer.createElement(sourroundBy);
+
+    // Add the div, just before the input
+    this.renderer.insertBefore(parent, liElement, this.el.nativeElement);
+
+    // Remove the input
+    this.renderer.removeChild(parent, this.el.nativeElement);
+
+    // Remove the directive attribute (not really necessary, but just to be clean)
+    this.renderer.removeAttribute(this.el.nativeElement, 'toolbarActionButton');
+
+    // Re-add it inside the div
+    this.renderer.appendChild(liElement, this.el.nativeElement);
+  }
 }
